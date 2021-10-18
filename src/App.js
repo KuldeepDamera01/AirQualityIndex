6daf72c4fd6825bcd72abcd0731053b287fa14ce
KuldeepDamera01 <88116692+KuldeepDamera01@users.noreply.github.com>
2021-10-17 22:49:54 -0700

    Add files via upload
    Commiting changes related to Air quality index application usinh reactjs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SearchCities from './SearchCities';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CENTRAL POLLUTION CONTROL BOARD'S</h1>
+      <h2 >AIR QUALITY STANDARDS</h2>
+      <SearchCities />
+    </div>
+  );
+}
+
+export default App;
